d9946a6129060bf3747426e8a75275c886c7b8c8
AleksaT <aleksa.topalovic015@gmail.com>
2015-05-08 20:52:08 +0200

    Prebacene metode iz prozora MenjacnicaGUI u GUIKontroler i izvrsena njihova podesavanja.

---
+++ Menjacnica/src/menjacnica/gui/GUIKontroler.java
@@ -0,0 +1,110 @@
+package menjacnica.gui;
+
+import java.awt.EventQueue;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import menjacnica.Menjacnica;
+import menjacnica.MenjacnicaInterface;
+import menjacnica.Valuta;
+
+public class GUIKontroler {
+
+	private static MenjacnicaGUI glavniProzor;
+	private static MenjacnicaInterface menjacnicaSistem;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					glavniProzor = new MenjacnicaGUI();
+					menjacnicaSistem = new Menjacnica();
+					glavniProzor.setVisible(true);
+					glavniProzor.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	public static void ugasiAplikaciju() {
+		int opcija = JOptionPane.showConfirmDialog(glavniProzor.getContentPane(),
+				"Da li ZAISTA zelite da izadjete iz apliacije", "Izlazak",
+				JOptionPane.YES_NO_OPTION);
+
+		if (opcija == JOptionPane.YES_OPTION)
+			System.exit(0);
+	}
+	
+	public static void prikaziAboutProzor(){
+		JOptionPane.showMessageDialog(glavniProzor.getContentPane(),
+				"Autor: Bojan Tomic, Verzija 1.0", "O programu Menjacnica",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	
+	public static void sacuvajUFajl() {
+		try {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showSaveDialog(glavniProzor.getContentPane());
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+
+				menjacnicaSistem.sacuvajUFajl(file.getAbsolutePath());
+			}
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(glavniProzor.getContentPane(), e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	/*
+	 * proveriti da li treba da prikaze sve valute
+	 * glavniProzor.prikaziSveValute(file);
+	 */
+	public static void ucitajIzFajla() {
+		try {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showOpenDialog(glavniProzor.getContentPane());
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				menjacnicaSistem.ucitajIzFajla(file.getAbsolutePath());
+				
+			}	
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(glavniProzor.getContentPane(), e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	public static void prikaziDodajKursGUI() {
+		DodajKursGUI prozor = new DodajKursGUI();
+		prozor.setLocationRelativeTo(glavniProzor.getContentPane());
+		prozor.setVisible(true);
+	}
+	
+	
+	public static void prikaziObrisiKursGUI(Valuta valuta) {
+		if (valuta != null) {
+			ObrisiKursGUI prozor = new ObrisiKursGUI(valuta);
+			prozor.setLocationRelativeTo(glavniProzor.getContentPane());
+			prozor.setVisible(true);
+		}
+	}
+	
+	
+	public static void prikaziIzvrsiZamenuGUI(Valuta valuta) {
+		if (valuta != null) {
+			IzvrsiZamenuGUI prozor = new IzvrsiZamenuGUI(valuta);
+			prozor.setLocationRelativeTo(glavniProzor.getContentPane());
+			prozor.setVisible(true);
+		}
+	}
+}
